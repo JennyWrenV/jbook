bf47dfd6dfc8f3ffc127964a4282e2ed298fa052
Wren <jenny@valvatne.com>
2022-08-02 14:25:30 +0200

    adds css

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,23 +1,68 @@
-import MonacoEditor from '@monaco-editor/react';
+import './code-editor.css';
+import MonacoEditor, { Monaco } from "@monaco-editor/react";
+import { useRef } from 'react';
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 
-const CodeEditor = () => {
-    return <MonacoEditor
-        theme="vs-dark"
-        language="javascript"
-        height="500px"
-        options={{
-            wordWrap: 'on',
-            minimap: { enabled: false },
-            showUnused: false,
-            folding: false,
-            lineNumbersMinChars: 3,
-            fontSize: 16,
-            scrollBeyondLastLine: false,
-            automaticLayout: true,
-            tabSize: 2
+interface CodeEditorProps {
+    initialValue: string;
+    onChange(value: string): void;
+}
+
+const CodeEditor = ({ onChange, initialValue }: CodeEditorProps) => {
+    const editorRef = useRef<monaco.editor.IStandaloneCodeEditor>();
+
+
+
+    const onEditorDidMount = (
+        editor: monaco.editor.IStandaloneCodeEditor,
+        _monaco: Monaco
+    ) => {
+        editorRef.current = editor;
+        editor.onDidChangeModelContent(() => onChange(editor.getValue()));
+        editor.getModel()?.updateOptions({ tabSize: 2 });
+    };
+
+    const onFormatClick = () => {
+        if (editorRef.current) {
+            const unformatted = editorRef.current.getValue()
+
+            const formatted = prettier.format(unformatted, {
+                parser: 'babel',
+                plugins: [parser],
+                useTabs: false,
+                semi: true,
+                singleQuote: true
+
+            }).replace(/\n$/, '')
+
+            editorRef.current?.setValue(formatted)
         }
+    }
 
-        } />;
+    return (
+        <div className='editor-wrapper'>
+            <button className="button button-format is-primary is-small" onClick={onFormatClick}>Format</button>
+            <MonacoEditor
+                onMount={onEditorDidMount}
+                value={initialValue}
+                theme='vs-dark'
+                language='javascript'
+                height='500px'
+                options={{
+                    wordWrap: "on",
+                    minimap: { enabled: false },
+                    showUnused: false,
+                    folding: false,
+                    lineNumbersMinChars: 3,
+                    fontSize: 16,
+                    scrollBeyondLastLine: false,
+                    automaticLayout: true,
+                }}
+            />
+        </div>
+    );
 };
 
 export default CodeEditor;
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import * as esbuild from 'esbuild-wasm';
 import React, { useState, useEffect, useRef } from 'react';
 import { createRoot } from "react-dom/client"
@@ -69,7 +70,10 @@ const App = () => {
 
   return (
     <div>
-      <CodeEditor />
+      <CodeEditor 
+      initialValue='const a = 1'
+      onChange={(value) => setInput(value)}
+      />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
