005f9dd7623a4528d321d0ef59da9204d3e0cfa0
Wren <jenny@valvatne.com>
2023-01-11 15:57:40 +0100

    adds cli functionality

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,8 +1,14 @@
 import { Command } from "commander";
+import { serve } from 'local-api'
+import path from 'path'
 
 export const serveCommand = new Command()
-.command('serve')
-.description('Open a file for editing')
-.action(()=> {
-    console.log('Getting ready to serve a file')
-})
+    .command('serve [filename]')
+    .description('Open a file for editing')
+    .option('-p, --port <number>', 'port to run server', '4005')
+    .action((filename = 'notebook.js', options: { port: string }) => {
+        const dir = path.join(process.cwd(), path.dirname(filename))
+
+
+        serve(parseInt(options.port), path.basename(filename), dir)
+    })
